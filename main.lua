60b075daefb896ebf6318ec15e5f9552675b91ab
kloggson <59363488+kloggson@users.noreply.github.com>
2019-12-30 13:56:17 +0100

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,75 @@
+local frame = KLGFrame or CreateFrame("FRAME", "KLGFrame");
+frame:RegisterEvent("CHAT_MSG_COMBAT_HONOR_GAIN");
+frame:RegisterEvent("ADDON_LOADED");
+
+local function parseHonorMessage(arg1)
+  local honor_gain_pattern = string.gsub(COMBATLOG_HONORGAIN, "%(", "%%(")
+  honor_gain_pattern = string.gsub(honor_gain_pattern, "%)", "%%)")
+  honor_gain_pattern = string.gsub(honor_gain_pattern, "(%%s)", "(.+)")
+  honor_gain_pattern = string.gsub(honor_gain_pattern, "(%%d)", "(%%d+)")
+  local victim, rank, est_honor = arg1:match(honor_gain_pattern)
+  if (victim) then
+    est_honor = math.max(0, math.floor(est_honor * (1 - 0.25 * ((KLGHonorDB.killed_players[victim] or 1) - 1)) + 0.5))
+  end
+
+  local honor_award_pattern = string.gsub(COMBATLOG_HONORAWARD, "(%%d)", "(%%d+)")
+  local awarded_honor = arg1:match(honor_award_pattern)
+  return victim, est_honor, awarded_honor
+end
+
+local function KLGHonor(self, event, arg1, ...)
+  if event == "ADDON_LOADED" and arg1 == "KLGHonor" then
+    -- denna event körs när ditt addon laddat alla filer
+    if KLGHonorDB == nil then
+      -- skapa ditt table (databasen här om den inte redan är skapad) sparas i din SavedVariables
+      KLGHonorDB = {
+        honor = 0,
+        reset_time = 0,
+        total_kills = 0,
+        lifetime_kills = 0,
+        lifetime_honor = 0,
+        killed_players = {}
+
+
+      }
+    end
+  elseif event == "CHAT_MSG_COMBAT_HONOR_GAIN" then
+    -- gör honor stuff här
+    local victim, _, awarded_honor = parseHonorMessage(arg1)
+    if victim then
+      KLGHonorDB.killed_players[victim] = (KLGHonorDB.killed_players[victim] or 0) + 1
+      local _, est_honor = parseHonorMessage(arg1)
+      KLGHonorDB.honor = KLGHonorDB.honor + est_honor
+      KLGHonorDB.lifetime_honor = KLGHonorDB.lifetime_honor + est_honor
+      KLGHonorDB.total_kills = KLGHonorDB.total_kills + 1
+      KLGHonorDB.lifetime_kills = KLGHonorDB.lifetime_kills + 1
+    elseif awarded_honor then
+
+    end
+  end
+end
+
+SLASH_KLG1 = "/klg"
+SlashCmdList["KLG"] = function(functionName)
+  local command, arg1, arg2 = strsplit(" ", functionName, 3);
+  if command == "test" then
+    -- triggas ingame av /klg test
+    print(KLGHonorDB.honor)
+  elseif command == "report" then
+    if (arg1) then
+      SendChatMessage("Honor gained since last reset: " .. KLGHonorDB.honor .. " from " .. KLGHonorDB.total_kills .. " kills.", arg1, nil, nil)
+    else
+      print("Honor gained since last reset: " .. KLGHonorDB.honor .. " from " .. KLGHonorDB.total_kills .. " kills.")
+    end
+  elseif command == "reset" then
+    print("All data has been reset")
+    KLGHonorDB = {
+      -- lägg till kill count
+      total_kills = 0,
+      honor = 0,
+      reset_time = 0,
+      killed_players = {}
+    }
+  end
+end
+frame:SetScript("OnEvent", KLGHonor);
